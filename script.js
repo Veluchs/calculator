2d6d720d86e3ed8959ecd226acc29e7210d82a18
Veluchs <mysterioso@mailbox.org>
2023-09-16 16:45:10 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    return operator(firstNumber, secondNumber);
+}
+
+function refreshScreen() {
+    screen.textContent = displayValue;
+}
+
+function selectNumber() {
+    let operators = "*%-+";
+    if (operators.includes(displayValue)) {
+        displayValue = "";
+    }
+    let number = this.textContent;
+    displayValue += number;
+    refreshScreen();
+}
+
+function clearCalculatorScreen() {
+    displayValue = "";
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    refreshScreen();
+}
+
+function deleteLastInput() {
+    displayValue = displayValue.slice(0, -1);
+    refreshScreen();
+}
+
+function selectOperator() {
+    if (!operator) {
+        firstNumber = Number(displayValue);
+    }
+    if (operator && firstNumber) {
+        secondNumber = Number(displayValue);
+        firstNumber = operate(operator, firstNumber, secondNumber);
+    }
+    operator = operatorObj[this.textContent];
+    displayValue = this.textContent;
+    refreshScreen();
+}
+
+function calculate() {
+    if (!operator || !firstNumber) {
+        return;
+    }
+    secondNumber = Number(displayValue);
+    result = operate(operator, firstNumber, secondNumber);
+    displayValue = result;
+
+    firstNumber = result;
+    operator = null;
+    secondNumber = null;
+
+    refreshScreen();
+}
+
+let firstNumber, secondNumber, operator;
+let displayValue = "";
+
+const screen = document.querySelector("#screen");
+const operatorObj = {
+    "-": substract,
+    "+": add,
+    "*": multiply,
+    "%": divide,
+};
+
+document.querySelectorAll(".number").forEach( button => button.addEventListener("click", selectNumber));
+document.querySelectorAll(".operator").forEach( button => button.addEventListener("click", selectOperator));
+document.querySelector("#clear").addEventListener("click", clearCalculatorScreen);
+document.querySelector("#delete").addEventListener("click", deleteLastInput);
+document.querySelector("#equal").addEventListener("click", calculate);
+
